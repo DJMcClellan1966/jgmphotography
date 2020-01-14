9626ddc7f0b3345471ffd9f023f7d482abcb54df
Douglas McClellan <DJMcClellan1966@gmail.com>
2020-01-14 14:50:55 -0500

    add portfolio and blog files to get up home webpage, needs tweaking to add jgm

--- jgmphotgraphy/settings.py
+++ jgmphotgraphy/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'jgmphotgraphy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jgmphotosdb',
+        'USER': 'postgres',
+        'PASSWORD': 'padrep10',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -117,4 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+
+]
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
---
+++ portfolio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from .models import Portfolio
+
+def home(request):
+    portfolios = Portfolio.objects
+    return render(request, 'jobs/home.html', {'portfolios':portfolios})
