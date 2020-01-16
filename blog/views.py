2c461a1a8727d7c7092bc65411b25836e596a260
Douglas McClellan <DJMcClellan1966@gmail.com>
2020-01-16 09:24:09 -0500

    updated blog urls and views

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 def allblogs(request):
-    blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+    return render(request, 'blog/allblogs.html')
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
