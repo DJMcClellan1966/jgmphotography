09936464623f774a5cfc492e9721082a6ae74408
Douglas McClellan <DJMcClellan1966@gmail.com>
2020-01-18 21:52:23 -0500

    added signup, log in and log out to home

--- jgmphotgraphy/urls.py
+++ jgmphotgraphy/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', portfolio.views.home, name ='home'),
     path('portfolio/', include('portfolio.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
